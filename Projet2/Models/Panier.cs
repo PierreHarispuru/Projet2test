262d44f543bbceabcaf49948628b890b1df1e957
MistoganErza <phungwingkit@gmail.com>
2022-11-03 09:44:57 +0100

    Jeanpierre

---
+++ Projet2/Models/Panier.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Projet2.Models
+{
+    public class Panier
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Numéro de produit")]
+        public int? ProdId { get; set; }
+        [Display(Name = "Qté")]
+        public int QuantitePanier { get; set; }
+        [Display(Name = "Description des produits")]
+        public string description { get; set; }
+        public string lienImage { get; set; }
+        [Display(Name = "Prix (€)")]
+        public double prix { get; set; }
+    }
+}
