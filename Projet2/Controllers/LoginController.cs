aaef8b34fe187cf9d7235b7a4cae7b94a800d561
MistoganErza <phungwingkit@gmail.com>
2022-11-07 09:49:17 +0100

    Adherer-faq-login

--- Projet2/Controllers/LoginController.cs
+++ Projet2/Controllers/LoginController.cs
@@ -1,6 +1,93 @@
-﻿namespace Projet2.Controllers
+﻿using Projet2.Models;
+using Projet2.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Projet2.Controllers
 {
-    public class LoginController
+    public class LoginController : Controller
     {
+        private Dal dal;
+        public LoginController()
+        {
+            dal = new Dal();
+        }
+        public IActionResult Index()
+        {
+            ProfilViewModel viewModel = new ProfilViewModel { Authentifie = HttpContext.User.Identity.IsAuthenticated };
+            if (viewModel.Authentifie)
+            {
+                viewModel.Profil = dal.ObtenirUtilisateur(HttpContext.User.Identity.Name);
+                return View(viewModel);
+            }
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(ProfilViewModel viewModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                Profil profil = dal.Authentifier(viewModel.Profil.Prenom, viewModel.Profil.Password);
+                if (profil != null)
+                {
+                    var userClaims = new List<Claim>()
+                    {
+                        new Claim(ClaimTypes.Name, profil.Id.ToString())
+                    };
+
+                    var ClaimIdentity = new ClaimsIdentity(userClaims, "User Identity");
+
+                    var userPrincipal = new ClaimsPrincipal(new[] { ClaimIdentity });
+
+                    HttpContext.SignInAsync(userPrincipal);
+
+                    if (!string.IsNullOrWhiteSpace(returnUrl) && Url.IsLocalUrl(returnUrl))
+                        return Redirect(returnUrl);
+
+                    return Redirect("/");
+                }
+                ModelState.AddModelError("Utilisateur.Prenom", "Prénom et/ou mot de passe incorrect(s)");
+            }
+            return View(viewModel);
+        }
+
+        public IActionResult CreerCompte()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreerCompte(Profil profil)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = dal.AjouterUtilisateur(profil.Prenom, profil.Password);
+
+                var userClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, id.ToString()),
+                };
+
+                var ClaimIdentity = new ClaimsIdentity(userClaims, "User Identity");
+
+                var userPrincipal = new ClaimsPrincipal(new[] { ClaimIdentity });
+                HttpContext.SignInAsync(userPrincipal);
+
+                return Redirect("/");
+            }
+            return View(profil);
+        }
+
+        public ActionResult Deconnexion()
+        {
+            HttpContext.SignOutAsync();
+            return Redirect("/");
+        }
     }
 }
--- Projet2/Models/IDal.cs
+++ Projet2/Models/IDal.cs
@@ -7,5 +7,10 @@ public interface IDal : IDisposable
     void DeleteCreateDatabase();
     List<Profil> ObtientTousLesProfils();
     int CreerProfil(Profil profil);
+
+    int AjouterUtilisateur(string nom, string password);
+    Profil Authentifier(string nom, string password);
+    Profil ObtenirUtilisateur(int id);
+    Profil ObtenirUtilisateur(string idStr);
     //void ModifierProfil(int id, string nom, string prenom, string mail, int telephone, string addresse, int codepostal);
 }
